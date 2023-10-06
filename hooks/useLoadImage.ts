308c6dd4672f042b22569ace9b81f93586ed8091
manseerat21 <smanseeratsinghgill21@gmail.com>
2023-10-06 15:13:30 +0530

    modified:   hooks/useLoadImage.ts

--- hooks/useLoadImage.ts
+++ hooks/useLoadImage.ts
@@ -1,19 +1,19 @@
-import { Song } from "@/types";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { Song } from "@/types";
 
 const useLoadImage = (song: Song) => {
-    const supabaseClient = useSupabaseClient();
+  const supabaseClient = useSupabaseClient();
+  
+  if (!song) {
+    return null;
+  }
 
-    if(!song) {
-        return null;
-    }
+  const { data: imageData } = supabaseClient
+    .storage
+    .from('images')
+    .getPublicUrl(song.image_path);
 
-    const { data: imageData } = supabaseClient
-        .storage
-        .from('images')
-        .getPublicUrl(song.image_path);
-
-    return imageData.publicUrl;
+  return imageData.publicUrl;
 };
 
 export default useLoadImage;
